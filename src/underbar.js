21e2b70bf020daee33725511d14e7c8171c6c452
ethoreby <ethoreby@gmail.com>
2014-01-17 11:55:25 -0800

    "reject passed"

--- src/underbar.js
+++ src/underbar.js
@@ -89,6 +89,13 @@ var _ = { };
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+    collection.forEach(function(n){
+      if((_.filter(collection, test).indexOf(n) === -1)) {
+        result.push(n);
+      }
+    });
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
